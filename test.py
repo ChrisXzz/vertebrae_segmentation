50cca3e9ab7cfbea257e1ec8e9f4979b62d812e0
MENG Di <di.meng@inria.fr>
2022-01-31 16:19:54 +0100

    allow input to be a folder of CTs

--- test.py
+++ test.py
@@ -42,9 +42,10 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser(description='Run pipeline on a single CT scan.')
-    parser.add_argument('-D', '--data_file', type=str, help='a CT scan in nifti format')
+    parser.add_argument('-D', '--input_data', type=str, help='a CT scan or a folder of CT scans in nifti format')
     parser.add_argument('-S', '--save_folder',  default='-1', type=str, help='folder to save the results')
     parser.add_argument('-F', '--force_recompute', action='store_true', help='set True to recompute and overwrite the results')
+    parser.add_argument('-L', '--initial_locations', action='store_true', help='set True to use initial location predictions')
     args = parser.parse_args()
 
 
@@ -61,67 +62,80 @@ if __name__ == "__main__":
     ### load trained models
     models = load_models()
 
-    ### check results existence
-    scanname = os.path.split(args.data_file)[-1].split('.')[0]
-    print(' ... checking: ', scanname)
 
-    if os.path.exists(os.path.join(save_folder, '{}_seg.nii.gz'.format(scanname))) and not args.force_recompute:
-        sys.exit(' ... {} result exists, not overwriting '.format(scanname))
+    ### inputs
+    scan_list = []
+    if os.path.isdir(args.input_data):
+        scan_list = glob.glob(os.path.join(args.input_data, '*.nii.gz'))
+    elif os.path.isfile(args.input_data):
+        scan_list.append(args.input_data)
+    else:
+        print('It is a special file (socket, FIFO, device file)')
 
-    print(' ... starting to process: ', scanname)
+
+    for scan_file in scan_list:
+        ### check results existence
+        scanname = os.path.split(scan_file)[-1].split('.')[0]
+        print(' ... checking: ', scanname)
+
+        if os.path.exists(os.path.join(save_folder, '{}_seg.nii.gz'.format(scanname))) and not args.force_recompute:
+            sys.exit(' ... {} result exists, not overwriting '.format(scanname))
+
+        print(' ... starting to process: ', scanname)
 
 
+        # =================================================================
+        # Load the CT scan 
+        # =================================================================
 
-    # =================================================================
-    # Load the CT scan 
-    # =================================================================
+        ### TODO: data I/O for other formats
 
-    ### TODO: data I/O for other formats
+        try:
+            pir_img = read_isotropic_pir_img_from_nifti_file(scan_file)
+        except ImageFileError:
+            sys.exit('The input CT should be in nifti format.')
 
-    try:
-        pir_img = read_isotropic_pir_img_from_nifti_file(args.data_file)
-    except ImageFileError:
-        sys.exit('The input CT should be in nifti format.')
+        print(' ... loaded CT volume in isotropic resolution and PIR orientation ')
 
-    print(' ... loaded CT volume in isotropic resolution and PIR orientation ')
 
-    # =================================================================
-    # Spine binary segmentation
-    # =================================================================
+        # =================================================================
+        # Spine binary segmentation
+        # =================================================================
 
-    from segment_spine import binary_segmentor
-    binary_mask = binary_segmentor(pir_img, models['seg_binary'], mode='overlap', norm=False)
+        from segment_spine import binary_segmentor
+        binary_mask = binary_segmentor(pir_img, models['seg_binary'], mode='overlap', norm=False)
 
-    print(' ... obtained spine binary segmentation ')
+        print(' ... obtained spine binary segmentation ')
 
-    # =================================================================
-    # Initial locations
-    # =================================================================
+        # =================================================================
+        # Initial locations
+        # =================================================================
 
-    locations = []
-    from locate import locate 
-    locations = locate(pir_img, models['loc_sagittal'], models['loc_coronal'])
+        locations = np.array([])
+        if args.initial_locations:
+            from locate import locate 
+            locations = locate(pir_img, models['loc_sagittal'], models['loc_coronal'])
 
-    print(' ... obtained {} initial 3D locations '.format(len(locations)))
+        print(' ... obtained {} initial 3D locations '.format(len(locations)))
 
-    # =================================================================
-    # Consistency circle - Locations refine - multi label segmentation 
-    # =================================================================
+        # =================================================================
+        # Consistency circle - Locations refine - multi label segmentation 
+        # =================================================================
 
-    from consistency_loop import consistency_refinement_close_loop
+        from consistency_loop import consistency_refinement_close_loop
 
-    multi_label_mask, locations, labels, loc_has_converged = consistency_refinement_close_loop(locations, pir_img, binary_mask,
-                                                                        models['seg_individual'],
-                                                                        models['id_group'], models['id_cervical'],
-                                                                        models['id_thoracic'], models['id_lumbar'])            
+        multi_label_mask, locations, labels, loc_has_converged = consistency_refinement_close_loop(locations, pir_img, binary_mask,
+                                                                            models['seg_individual'],
+                                                                            models['id_group'], models['id_cervical'],
+                                                                            models['id_thoracic'], models['id_lumbar'])            
 
-    print(' ... obtained PIR multi label segmentation ')
+        print(' ... obtained PIR multi label segmentation ')
 
-    # =================================================================
-    # Save the result in original format
-    # =================================================================
+        # =================================================================
+        # Save the result in original format
+        # =================================================================
 
-    from utils import get_size_and_spacing_and_orientation_from_nifti_file, write_result_to_file
-    ori_size, ori_spacing, ori_orient_code, ori_aff = get_size_and_spacing_and_orientation_from_nifti_file(args.data_file)
+        from utils import get_size_and_spacing_and_orientation_from_nifti_file, write_result_to_file
+        ori_size, ori_spacing, ori_orient_code, ori_aff = get_size_and_spacing_and_orientation_from_nifti_file(scan_file)
 
-    write_result_to_file(multi_label_mask, ori_orient_code, ori_spacing, ori_size, ori_aff, save_folder, scanname)
+        write_result_to_file(multi_label_mask, ori_orient_code, ori_spacing, ori_size, ori_aff, save_folder, scanname)
